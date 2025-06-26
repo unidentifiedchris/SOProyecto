361b37b7ea943ede77ac5011922e4294123ee3ad
Christopher <49421186+unidentifiedchris@users.noreply.github.com>
2025-06-26 04:36:12 +0000

    [ADD] Build, initial test

---
+++ build/CMakeFiles/flujo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flujo.dir/src/main.cpp.o"
+  "CMakeFiles/flujo.dir/src/main.cpp.o.d"
+  "flujo"
+  "flujo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flujo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/flujo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for flujo.
+# This may be replaced when dependencies are built.
